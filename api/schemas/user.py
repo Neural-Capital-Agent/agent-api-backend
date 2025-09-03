4274bc6dc25d8802355348cea6efdbfa0e876872
Lucas Salomon Quijano <lucassalomon28@hotmail.com>
2025-09-03 01:18:40 +0000

    add users logic

--- api/schemas/user.py
+++ api/schemas/user.py
@@ -1,5 +1,24 @@
 from pydantic import BaseModel, EmailStr
+from typing import Optional
 
 class UserCreate(BaseModel):
+    email: Optional[EmailStr] = None
+    phone: str
+    street_address: str
+    city: str
+    state: str
+    postal_code: str
+    given_name: str
+    family_name: str
+    date_of_birth: str
+    country_of_citizenship: str
+    country_of_birth: str
+    tax_id: str
+    contact_given: str
+    contact_family: str
+    contact_email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
     email: EmailStr
-    name: str
+    password: str
